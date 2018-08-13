e329ea54ff439611253d906166b3bdfdce2110cf
Deepak Nathani <deepakn1019@gmail.com>
2018-08-13 16:11:17 +0530

    Added support for Ripser

---
+++ src_ripser/n_hop_multi.cpp
@@ -0,0 +1,248 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <set>
+#include <queue>
+#include <tuple>
+#include <map>
+#include <limits>
+#include <utility>
+
+using namespace std;
+
+#define intt int64_t
+#define ii pair<intt, intt>
+
+ifstream iFile;
+ofstream oFile;
+string dataset_name;
+
+vector<intt> bfs(vector<vector< ii > >& in, vector<intt> sources, intt num_nodes){
+	
+	vector<intt> len(num_nodes+1,INT64_MAX); // len[i]-> length of path from source to node i
+	vector<bool> visit(num_nodes,0); // visited array 
+
+	queue<int> q;
+	for(int i = 0; i < sources.size(); i++){
+		q.push(sources[i]); 
+		visit[sources[i]] = 1;
+		len[sources[i]] = 0;
+	}
+
+	while(!q.empty()){
+
+		int u = q.front(); // get current node
+
+		for(int i = 0; i < in[u].size(); i++){		
+			
+			if(visit[in[u][i].second] == 0){
+				q.push(in[u][i].second); // push the child node in the queue
+				visit[in[u][i].second] = 1; // mark the node visited
+				len[in[u][i].second] = len[u]+1; // update length from source to current child node
+			}
+
+		}
+
+		q.pop();
+	}
+
+	return len;
+}
+
+intt input(vector<vector< ii > >& in, map<string,intt>& to_indices, map<intt,string>& to_node){
+	string u; 
+	intt edges = 0;
+	int num_sources = 0;
+	intt i = 1;
+	in.resize(1);
+
+	string source, dest;
+
+	while(true){
+		string u;
+		// cin >> u;
+		iFile >> u; // read from file
+
+		if(iFile.eof()){
+			break; // break it end of file
+		}
+
+		else{
+
+			// check for the source node
+			if(u[u.length()-1] == ':'){ 
+				string un;
+				un = u.substr(0, u.length()-1); // remove colon
+				source = un;
+				if(to_indices.find(source) == to_indices.end()){
+					to_indices[source] = i;
+					to_node[i] = source;
+					vector< ii > tmp;
+					in.push_back(tmp);
+					i++;
+				}
+
+			}
+
+			else{
+				edges++;
+				intt weight;
+				dest = u;
+
+				if(to_indices.find(dest) == to_indices.end()){
+					to_indices[dest] = i;
+					to_node[i] = dest;
+					vector< ii > tmp;
+					in.push_back(tmp);
+					i++;
+				}
+				iFile >> weight;
+				in[to_indices[source]].push_back(make_pair(weight, to_indices[dest])); 
+				// if(find(in[to_indices[dest]].begin(), in[to_indices[dest]].end(), make_pair(weight, to_indices[source])) == in[to_indices[dest]].end())
+				// 	in[to_indices[dest]].push_back(make_pair(weight, to_indices[source]));
+			}	
+		}
+	}
+	// cout << "Edges: " << edges << '\n';
+	return i-1; // number of distinct nodes
+}
+
+void write_complete(intt distinct_nodes, int hop, vector<intt>& sources, map<intt,string>& to_node, bool remove){
+	string out_path = "/home/deepak/Project/files/outputs/"+dataset_name+"/n_" + to_string(hop) + "/apsp_complete_full_" + to_node[sources[0]] + "_" + to_node[sources[1]];
+	if(remove){
+		out_path = "/home/deepak/Project/files/outputs/"+dataset_name+"/removed_edge_" + to_string(hop) + "/apsp_complete_full_" + to_node[sources[0]] + "_" + to_node[sources[1]];
+	}
+
+	ofstream cFile(out_path, ios::binary);
+
+	intt DIPHA = 8067171840,file_type = 7;
+	cFile.write(reinterpret_cast<const char*>(&DIPHA), sizeof(intt));
+	cFile.write(reinterpret_cast<const char*>(&file_type), sizeof(intt));
+	cFile.write(reinterpret_cast<const char*>(&distinct_nodes), sizeof(intt));
+
+	for(int i = 0; i < distinct_nodes; i++){
+		for(int j = 0; j < distinct_nodes; j++){
+			double distance = (double)(1-(i==j));
+			cFile.write(reinterpret_cast<const char*>(&distance), sizeof(double));
+		}
+	}
+	cFile.close();
+}
+
+intt write_txt(intt num_nodes, intt hop, vector<intt>& dist, vector<vector< ii > >& in, map<intt,string> to_node){	
+	
+	set<string> s;
+
+	for(intt v = 1; v <= num_nodes; v++){
+		if(dist[v] <= hop){
+			oFile << to_node[v] << ":\n";
+			s.insert(to_node[v]);
+			for(int i = 0; i < in[v].size(); i++){
+				if(dist[in[v][i].second] <= hop){
+					s.insert(to_node[in[v][i].second]);
+					oFile << to_node[in[v][i].second] << " " << in[v][i].first <<'\n';
+				}
+			}
+		}
+	}
+	return s.size();
+}
+
+int main(int argc, char *argv[]){
+	string input_file;
+	string output_file;
+	string source_1;
+	string source_2 = "-1";
+	intt hop;
+	vector<intt> sources;
+	bool remove = false;
+
+	if(argc != 6 and argc != 7 and argc != 8){
+		cout << "[Usage]: " << "./a.out dataset_name data_filename output_filename source_1 number_of_hops" << '\n';
+		cout << "[Usage]: " << "./a.out dataset_name data_filename output_filename source_1 source_2 number_of_hops" << '\n';
+		cout << "Options: \n";
+		cout << "--remove -> Used when the input data is modified by removing an edge\n";
+		return 0;
+	}
+	else if(argc == 6){
+		dataset_name = argv[1];
+		input_file = argv[2];
+		output_file = argv[3];
+		source_1 = argv[4];
+		hop = atoi(argv[5]);
+	}
+	else if(argc == 7){
+		int i = 1;
+		if((string)argv[1] == "--remove"){
+			remove = true;
+			i = 2;
+			dataset_name = argv[i];
+			input_file = argv[i+1];
+			output_file = argv[i+2];
+			source_1 = argv[i+3];
+			hop = atoi(argv[i+4]);
+		}
+		else{
+			dataset_name = argv[i];
+			input_file = argv[i+1];
+			output_file = argv[i+2];
+			source_1 = argv[i+3];
+			source_2 = argv[i+4];
+			hop = atoi(argv[i+5]);
+		}
+			
+	}
+	else{
+		remove = true;
+		int i = 2;
+		dataset_name = argv[i];
+		input_file = argv[i+1];
+		output_file = argv[i+2];
+		source_1 = argv[i+3];
+		source_2 = argv[i+4];
+		hop = atoi(argv[i+5]);
+	}
+
+	iFile.open((string) input_file);
+	oFile.open((string) output_file);
+
+	map<string,intt> to_indices; // maps node to index(1-n), index is always from 1-n where n is number of distinct nodes
+	map<intt,string> to_node; // reverse map tp obtain node number using it's index
+
+	vector<vector< ii > > in;
+
+	int num_nodes = input(in, to_indices, to_node); // take input
+	// cout << "Input done. "<< num_nodes << "\n";
+	
+	if(source_2 == "-1"){
+		if(to_indices.find(source_1) == to_indices.end()){
+			to_indices[source_1] = num_nodes+1;
+			to_node[num_nodes+1] = source_1;
+			num_nodes++;
+		}
+		sources.push_back(to_indices[source_1]);
+	}
+	else{
+		if(to_indices.find(source_1) == to_indices.end()){
+            to_indices[source_1] = num_nodes+1;
+            to_node[num_nodes+1] = source_1;
+            num_nodes++;
+        }
+		if(to_indices.find(source_2) == to_indices.end()){
+            to_indices[source_2] = num_nodes+1;
+            to_node[num_nodes+1] = source_2;
+            num_nodes++;
+        }
+		sources.push_back(to_indices[source_1]);
+		sources.push_back(to_indices[source_2]);
+	}
+
+// run bfs on the given nodes
+	vector<intt> out = bfs(in, sources, num_nodes);
+
+	intt distinct_nodes = write_txt(num_nodes, hop, out, in, to_node);
+	if(sources.size() == 2)
+		write_complete(distinct_nodes, hop, sources, to_node, remove);
+}
---
+++ src_ripser/script.py
@@ -0,0 +1,171 @@
+import sys
+import os
+import struct
+import pandas as pd
+import numpy as np
+from collections import OrderedDict
+from subprocess import Popen, PIPE
+# get input nodes and define neighbourhood
+# input filename
+# n-hop neighbourhood -- n
+
+def appendCSV(final_results, sep, out_file):
+	result = pd.DataFrame(final_results, columns = final_results[0].keys())
+	if(os.path.isfile(out_file)):
+		result.to_csv(out_file, mode = 'a', header = False, index = False)
+	else:
+		result.to_csv(out_file, sep = sep, index=False)
+
+def main():
+	if(len(sys.argv) != 5):
+		print("[Usage:] python3 script.py dataset_name pair_data output_file n")
+		exit()
+
+	dataset_name = sys.argv[1]
+	pair_data = sys.argv[2]
+	out_file = sys.argv[3]
+	hop = int(sys.argv[4])
+
+	data_file =  "/home/deepak/Project/files/data/"+dataset_name+"/data.txt"
+
+	if(os.path.exists("/home/deepak/Project/files/outputs/"+dataset_name+"/n_" + str(hop)) == False):
+		os.system("mkdir /home/deepak/Project/files/outputs/"+dataset_name+"/n_" + str(hop))
+
+	# compile all c++ files
+
+
+	# get all the reachable pairs in the graph to test
+
+	# os.system("/home/deepak/Project/code/src_server/johnson --dump_pairs " + dataset_name + " " + data_file + " /home/deepak/Project/files/outputs/"+dataset_name+"/n_" + str(hop) + "/global.txt /home/deepak/Project/files/outputs/"+dataset_name+"/n_" + str(hop) + "/global_sparse.txt" )
+
+	# data = "/home/deepak/Project/files/outputs/"+dataset_name+"/dumped.txt"
+	# data = "/home/deepak/Project/code/src_server/random_select.txt"
+
+	# define a list of ordered dict to save the results in excel
+	final_results = []
+	lines = []
+	total_pairs = 0
+
+	# open the data file and repeat the process for each pair
+	f = open(pair_data, "r")
+	lines = f.readlines()
+	lines = [l.strip().split() for l in lines]
+	f.close()
+
+	# if file exists, resume 
+	resume_pos = 0
+
+	if(os.path.isfile(out_file)):
+		df = pd.read_csv(out_file)
+		resume_pos = df.shape[0]
+		print(resume_pos)
+	
+	for l in lines:
+		node_a = l[1]
+		node_b = l[2]
+		temp = OrderedDict()
+		temp["node_a"] = node_a
+		temp["node_b"] = node_b
+		temp["distance"] = int(l[0])
+		total_pairs+=1
+
+		if(total_pairs <= resume_pos):
+			continue
+
+		# define file names for persistence diagrams
+
+		dgm1_file = "/home/deepak/Project/files/outputs/"+dataset_name+"/n_" + str(hop) + "/ripser_" + str(node_a) + ".txt"
+		dgm2_file = "/home/deepak/Project/files/outputs/"+dataset_name+"/n_" + str(hop) + "/ripser_" + str(node_b) + ".txt"
+		dgmCombine_file = "/home/deepak/Project/files/outputs/"+dataset_name+"/n_" + str(hop) + "/ripser_" + str(node_a) + "_" + str(node_b) + ".txt"
+		dgmComplete_file = "/home/deepak/Project/files/outputs/"+dataset_name+"/n_" + str(hop) + "/ripser_complete_" + str(node_a) + "_" + str(node_b) + ".txt"
+
+		# obtain persistence diagrams for node_a, node_b and combined
+
+		if(not os.path.isfile(dgm1_file)):
+			os.system("python3 /home/deepak/Project/code/src_server/get_persDiag.py " + dataset_name + " " + data_file + " " + str(node_a) + " " + str(hop))
+		if(not os.path.isfile(dgm2_file)):
+			os.system("python3 /home/deepak/Project/code/src_server/get_persDiag.py " + dataset_name + " " + data_file + " " + str(node_b) + " " + str(hop))
+
+		os.system("python3 /home/deepak/Project/code/src_server/get_persDiag.py " + dataset_name + " " + data_file + " " + str(node_a) + " " + str(node_b) + " " + str(hop))
+
+		# get persistence diagram for complete graph
+		in_file = "/home/deepak/Project/files/outputs/"+dataset_name+"/n_" + str(hop) + "/apsp_complete_full_" + str(node_a) + "_" + str(node_b)
+		# f = open(in_file, "rb")
+		# data = f.read()
+		# num_processors = struct.unpack('<q' , data[16:24])[0]
+		# if(num_processors > 20):
+		# 	num_processors = 20
+		# if(num_processors == 0):
+		# 	num_processors+=1
+		command = "ripser --dim 1 " + in_file + " > " + dgmComplete_file
+		os.system(command)			
+
+		# compare pairwise diagrams
+		process_a_b = Popen(["/home/deepak/Project/code/src_server/baseline", data_file, "/home/deepak/Project/code/src_server/test.txt", str(node_a), str(node_b)], stdout=PIPE)
+		process_a_0 = Popen(["python3", "/home/deepak/Project/code/src_server/compare_diagram.py", dgmCombine_file, dgm1_file, str(2),str(0)], stdout=PIPE)
+		process_a_1 = Popen(["python3", "/home/deepak/Project/code/src_server/compare_diagram.py", dgmCombine_file, dgm1_file, str(2),str(1)], stdout=PIPE)
+		process_b_0 = Popen(["python3", "/home/deepak/Project/code/src_server/compare_diagram.py", dgmCombine_file, dgm2_file, str(2),str(0)], stdout=PIPE)
+		process_b_1 = Popen(["python3", "/home/deepak/Project/code/src_server/compare_diagram.py", dgmCombine_file, dgm2_file, str(2),str(1)], stdout=PIPE)
+		process_complete_0 = Popen(["python3", "/home/deepak/Project/code/src_server/compare_diagram.py", dgmCombine_file, dgmComplete_file, str(2),str(0)], stdout=PIPE)
+		process_complete_1 = Popen(["python3", "/home/deepak/Project/code/src_server/compare_diagram.py", dgmCombine_file, dgmComplete_file, str(2),str(1)], stdout=PIPE)
+		
+		print(node_a, node_b)
+		(output_a_b,err) = process_a_b.communicate()
+		output_a_b = output_a_b.strip().splitlines()
+
+		(output_a_0,err) = process_a_0.communicate()
+		output_a_0 = output_a_0.strip().splitlines()
+
+		(output_a_1,err) = process_a_1.communicate()
+		output_a_1 = output_a_1.strip().splitlines()
+
+		(output_b_0,err) = process_b_0.communicate()
+		output_b_0 = output_b_0.strip().splitlines()
+
+		(output_b_1,err) = process_b_1.communicate()
+		output_b_1 = output_b_1.strip().splitlines()
+
+		(output_complete_0, err) = process_complete_0.communicate()
+		output_complete_0 = output_complete_0.strip().split()
+
+		(output_complete_1, err) = process_complete_1.communicate()
+		output_complete_1 = output_complete_1.strip().split()
+
+		# create a temporary dictionary
+			
+		temp["W_a_0"] = float(output_a_0[0])
+		temp["B_a_0"] = float(output_a_0[1])
+
+		temp["W_b_0"] = float(output_b_0[0])
+		temp["B_b_0"] = float(output_b_0[1])
+
+		temp["W_a_1"] = float(output_a_1[0])
+		temp["B_a_1"] = float(output_a_1[1])
+
+		temp["W_b_1"] = float(output_b_1[0])
+		temp["B_b_1"] = float(output_b_1[1])
+
+		temp["W_comp_0"] = float(output_complete_0[0])
+		temp["B_comp_0"] = float(output_complete_0[1])
+
+		temp["W_comp_1"] = float(output_complete_1[0])
+		temp["B_comp_1"] = float(output_complete_1[1])
+		
+		temp["AA"] = float(output_a_b[0])
+		temp["MW"] = float(output_a_b[1])
+
+		final_results.append(temp)
+
+		if(total_pairs%1 == 0):
+			print(total_pairs)
+			appendCSV(final_results, ',', out_file)
+			del final_results[:]
+
+	if(len(final_results) > 0):
+		appendCSV(final_results, ',', out_file)
+	sorted_result = pd.read_csv(out_file)
+	sorted_result.sort_values(by=['distance'], inplace = True, ascending = True)
+	sorted_result.to_csv(out_file, sep = ',')
+
+if __name__ == '__main__':
+	main()
